6680618c665b9d1948d6ec4586d4d2d1ce6e583c
cradulescu <seaman105@yahoo.com>
2020-11-21 19:35:43 +0200

    Add module dockermirror.py

---
+++ src/modules/dockermirror.py
@@ -0,0 +1,33 @@
+import docker
+import configparser
+
+
+def run(file):
+    # Create docker client
+    docker_client = docker.from_env()
+
+    # Read variables from config.ini. This implies that only config.ini should be modified for mirroring new images
+    config = configparser.ConfigParser()
+    config.read(file)
+
+    # Iterate trough config.ini
+    for section in config.sections():
+        items = dict(config[section])  # Create dictionary from config.ini sections
+        sr = items['source-repository']  # Create variable with the source from where the image will be pulled
+        tag = items['tag']  # Create a variable with the image tag
+        tr = items['target-repository']  # Create a variable with the target where the image will be pushed
+        image = items['image']  # Create variable with then name and tag of the pulled image
+        image_source = (sr + "/" + image + ":" + tag)  # Create variable for source image
+        image_target = (tr + "/" + image + ":" + tag)  # Create variable for source image
+
+        try:
+            docker_client.images.pull(image_source)  # Pull image from repository
+            pull_image = docker_client.images.get(image_source)  # Read source image from localhost
+            print('Successfully pulled image ' + image_source)
+            pull_image.tag(image_target)  # Tag image with repository target name and tag
+            print('Successfully tag image ', image_source, " to ", image_target, sep="")
+            docker_client.images.push(image_target)  # Push image to ecr
+            print('Successfully pushed image ', image_target, sep="")
+
+        except ValueError:
+            print(ValueError)
